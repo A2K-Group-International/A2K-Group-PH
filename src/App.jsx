d84b15a54c56dfa62646e8f3de7da4fe23d1cf6a
A2K-Group-International <a2kdev@a2kgroup.co.uk>
2024-07-15 02:34:25 +0800

    responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="font-primaryFont mx-auto max-w-screen-xl px-4 py-10 font-normal sm:px-6 md:px-8">
+    <div className="mx-auto max-w-screen-xl px-4 py-10 font-primaryFont font-normal sm:px-6 md:px-8">
       <Header />
+      <main className="h-svh bg-red-500"></main>
     </div>
   );
 }
---
+++ src/Components/Logo.jsx
@@ -0,0 +1,9 @@
+import A2KLogo from "../assets/vector/header-logo.svg";
+const Logo = () => {
+  return (
+    <a href="#">
+      <img src={A2KLogo} alt="A2K Group" />
+    </a>
+  );
+};
+export default Logo;
