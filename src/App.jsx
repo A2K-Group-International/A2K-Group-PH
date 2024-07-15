bb05acd48598f7e50a3835ed59db93dccec95419
A2K-Group-International <a2kdev@a2kgroup.co.uk>
2024-07-15 15:18:51 +0800

    Added gold banner on section1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./Components/Header";
+import Section1 from "./Components/Section1";
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-10 font-primaryFont font-normal sm:px-6 md:px-8">
+    <div className="mx-auto max-w-screen-xl px-4 py-10 font-primaryFont font-normal text-darkGray sm:px-6 md:px-8">
       <Header />
-      <main className="h-svh bg-red-500"></main>
+      <main>
+        <Section1 />
+      </main>
     </div>
   );
 }
---
+++ src/Components/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({ name }) => {
+  return (
+    <button className="rounded-full bg-darkGray px-4 py-2 text-sm font-[10px] text-white">
+      <a href="">{name}</a>
+    </button>
+  );
+};
+export default Button;
