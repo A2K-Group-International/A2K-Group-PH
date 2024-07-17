20308e1cd0cffb3cdc7d356cb42a2a9fdc254d19
Jose Castillo <josegabrielrcastillo@gmail.com>
2024-07-17 10:41:36 +0800

    change glass::before z-index to -1, change glass padding to 4, add border-radius to glass images.

--- src/Components/GlassBG.jsx
+++ src/Components/GlassBG.jsx
@@ -21,10 +21,10 @@ const GlassBG = () => {
     exitBeforeEnter: true,
   });
   return (
-    <div className="relative mt-10 h-[260px] w-[290px] rounded-3xl border-[0.5px] border-[#DCDCDC] bg-white p-5 shadow-2xl before:absolute before:left-1/2 before:top-1/2 before:z-[-2] before:min-h-[200px] before:min-w-[350px] before:-translate-x-1/2 before:-translate-y-1/2 before:transform before:rounded-3xl before:border-[0.5px] before:border-[#DCDCDC] before:bg-white/20 before:drop-shadow-sm before:backdrop-blur-lg before:content-[''] after:absolute after:left-1/2 after:top-1/2 after:z-[-1] after:min-h-[235px] after:min-w-[320px] after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-3xl after:border-[0.5px] after:border-[#DCDCDC] after:drop-shadow-sm after:backdrop-blur-sm after:content-[''] sm:h-[360px] sm:w-[507px] before:sm:h-[262px] before:sm:w-[571px] after:sm:h-[314px] after:sm:w-[539px]">
+    <div className="relative mt-10 h-[260px] w-[290px] rounded-3xl border-[0.5px] border-[#DCDCDC] bg-white p-4 shadow-2xl before:absolute before:left-1/2 before:top-1/2 before:z-[-1] before:min-h-[200px] before:min-w-[350px] before:-translate-x-1/2 before:-translate-y-1/2 before:transform before:rounded-3xl before:border-[0.5px] before:border-[#DCDCDC] before:bg-white/20 before:drop-shadow-sm before:backdrop-blur-sm before:content-[''] after:absolute after:left-1/2 after:top-1/2 after:z-[-1] after:min-h-[235px] after:min-w-[320px] after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-3xl after:border-[0.5px] after:border-[#DCDCDC] after:drop-shadow-sm after:backdrop-blur-sm after:content-[''] sm:h-[360px] sm:w-[507px] before:sm:h-[262px] before:sm:w-[571px] after:sm:h-[314px] after:sm:w-[539px]">
       {transitions((style, i) => (
         <animated.div
-          className="h-full bg-contain bg-center bg-no-repeat"
+          className="h-full bg-cover bg-center bg-no-repeat rounded-2xl"
           key={index}
           style={{
             ...style,
